7409b2357830c65f9b4b4c73679aa5cf08b606ce
hiijs <bernadetta.nycz@icloud.com>
2019-04-29 12:02:53 +0200

    Updates

---
+++ precache-manifest.c9a9de1d944917b6839bed869a7b46dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cf04aeb27454bcdf673",
+    "url": "/react-5-API/static/js/runtime~main.09a54eb4.js"
+  },
+  {
+    "revision": "5d1e1060d4de7a9d886d",
+    "url": "/react-5-API/static/js/main.2daa3f84.chunk.js"
+  },
+  {
+    "revision": "ffbe32d4e790e6a5d2f2",
+    "url": "/react-5-API/static/js/2.335f0c66.chunk.js"
+  },
+  {
+    "revision": "5d1e1060d4de7a9d886d",
+    "url": "/react-5-API/static/css/main.e641d22c.chunk.css"
+  },
+  {
+    "revision": "8140ed62e9ef0be944f0d558126e96a3",
+    "url": "/react-5-API/index.html"
+  }
+];
